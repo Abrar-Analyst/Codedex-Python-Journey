9ccb406131eae1c02f9bfb29c30e05652f3c4022
Abrar-Analyst <Abrarasiri017@gmail.com>
2026-02-07 11:16:30 +0300

    05. Snail Mail
    Instructions
    Create a blank letter.py program.
    
    In this exercise, you will write a letter to your future self... in Python.
    
    Take a moment and think about what you hope to achieve on this journey.
    
    Use print() to output:
    
    Today's date.
    How you are feeling right now.
    What you want to accomplish by learning to code.
    A message to your older, wiser, and programmer self.
    Your favorite emoji to spice things up!

---
+++ Snail mail.py
@@ -0,0 +1,6 @@
+import datetime
+print(datetime.date.today())
+print("I'm feeling excited and energezied to level up!")
+print("I want to be the strongest candidate in the interview day")
+print("I hope I'll never let you down")
+print("💜 always and forever")
